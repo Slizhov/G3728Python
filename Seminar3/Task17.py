9b8e175099ccc8c6c655170bca119bf700583138
Slizhov Mikhail <slizhov383@gmail.com>
2023-03-27 10:39:36 +0300

    add solution for 17,19,21,23

---
+++ Seminar3/Task17.py
@@ -0,0 +1,17 @@
+# Задача №17. 
+# Решение в группах
+# Дан список чисел. Определите, сколько в нем
+# встречается различных чисел.
+# Input: [1, 1, 2, 0, -1, 3, 4, 4]
+# Output: 6
+
+import random
+#my_list = [random.randint(0,10) for i in range(10)]
+
+my_list = []
+for i in range(20):
+    my_list.append(random.randint(0,10))
+
+print(my_list)
+print(set(my_list))
+print(len(set(my_list)))
